c4dbedee1ea7867ad5997f65bb7e355287437812
Unknown <margarida.rpds@gmail.com>
2019-11-15 15:37:22 +0000

    [Feat] Find Food

--- src/utils/Communication.java
+++ src/utils/Communication.java
@@ -10,7 +10,6 @@ public final class Communication {
         public static final String PREDATOR_FIND_MATE = "find-mate-predator-ontology";
         public static final String PREDATOR_REACHED_FEMALE = "reach-female-predator-ontology";
         public static final String FIND_FOOD = "find-food-ontology";
-        public static final String TELL_FOOD = "find-food-ontology";
     }
 
     // service types
