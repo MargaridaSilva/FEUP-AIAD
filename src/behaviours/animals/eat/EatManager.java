5b67c594fa2f74ab8fcd4b212da4ed5a7366df32
Cesar-Medeiros <cesar.mnm@gmail.com>
2019-11-17 13:03:16 +0000

    [Add] Eat Handler incomplete

--- src/behaviours/animals/eat/EatManager.java
+++ src/behaviours/animals/eat/EatManager.java
@@ -4,10 +4,17 @@ import agents.AnimalAgent;
 
 import behaviours.animals.BehaviourManager;
 import behaviours.animals.move.MoveToGoal;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+import sajas.core.behaviours.Behaviour;
 import sajas.core.behaviours.SequentialBehaviour;
 import sajas.core.behaviours.TickerBehaviour;
+import utils.Communication;
 import utils.Configs;
+import utils.Locator;
 import utils.Position;
+
+import java.io.IOException;
 
 public class EatManager extends TickerBehaviour {
     public Position food = new Position(0,0);
@@ -21,17 +28,44 @@ public class EatManager extends TickerBehaviour {
     @Override
     protected void onTick() {
 
-        SequentialBehaviour eat = new SequentialBehaviour(myAgent);
 
-        // Find Food
-        eat.addSubBehaviour(new FindFood(myAgent, FindFood.prepareRequest(myAgent), this));
+        if(onFood()){
+            //Eat
+            this.eatFood();
 
-        // Move Towards Food
-        eat.addSubBehaviour(new MoveToGoal(myAgent, food));
+        }else{
+            SequentialBehaviour eat = new SequentialBehaviour(myAgent);
 
-        // Eat
+            // Find Food
+            eat.addSubBehaviour(new FindFood(myAgent, FindFood.prepareRequest(myAgent), this));
 
-        this.parentBehaviour.addSubBehaviour(eat);
+            // Move Towards Food
+            eat.addSubBehaviour(new MoveToGoal(myAgent, food));
 
+            this.parentBehaviour.addSubBehaviour(eat);
+        }
     }
+
+    private void eatFood(){
+        AID observerAgent = Locator.findObserver(myAgent);
+        ACLMessage terminateMsg = new ACLMessage(ACLMessage.INFORM);
+        terminateMsg.setOntology(Communication.Ontology.HANDLE_EAT);
+        terminateMsg.addReceiver(observerAgent);
+        try {
+            terminateMsg.setContentObject(food);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        myAgent.send(terminateMsg);
+    }
+
+    private boolean onFood(){
+        AnimalAgent animalAgent = (AnimalAgent) this.myAgent;
+
+        if(animalAgent == null || this.food == null) return false;
+
+        return animalAgent.getPosition().equals(this.food);
+    }
+
+
 }
